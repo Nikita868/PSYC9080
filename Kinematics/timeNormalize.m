b6a07c36cb35529e2110b0288d693c67703bab5c
Nikita Kuznetsov <72103196+Nikita868@users.noreply.github.com>
2022-02-08 13:21:17 -0500

    Delete timeNormalize.m

--- Kinematics/timeNormalize.m
+++
@@ -1,8 +0,0 @@
-function [normData] = timeNormalize(data)
-
-% Cubic-fit spline interpolation. This normalizes the
-% timing across trials to 100 points. See dissertation by Yang (2007) p.
-% 126 for reference.
-
-normData = spline(1:length(data),data,linspace(1,length(data),100));
-end
